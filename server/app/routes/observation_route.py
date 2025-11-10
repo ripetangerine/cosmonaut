4d50df75232c66c7bbb9614e88ca1e8f6d6a4452
KwakJakyeong <ggyul847@gmail.com>
2025-11-11 03:43:29 +0900

    Feat(BE): 별 좌표 전달 기능 적위 적경을 LST로 변경

--- server/app/routes/observation_route.py
+++ server/app/routes/observation_route.py
@@ -1,8 +1,12 @@
+import json
 from fastapi import APIRouter, Query
 import schema.schema_manager as SchemaManager
 import config
 import httpx
-import datetime as date
+from datetime import datetime, timezone
+from math import sin, cos, asin, acos, pi
+from typing import List
+from pydantic import BaseModel
 
 # TODO : 프론트에서 진행
 # 프론트에서
@@ -61,7 +65,7 @@ async def observation(
   }
 
   # 하루치 데이터만 db에서 관리 : 날짜가 다르다면 db 삭제
-  if(date.today() != await db.custom("SELECT date_log FROM ObservationDailyLog ORDER BY date_log DESC LIMIT 1")):
+  if(datetime.today() != await db.custom("SELECT date_log FROM ObservationDailyLog ORDER BY date_log DESC LIMIT 1")):
     try:
       await db.delete("ObservationDailyLog")
     except Exception as e:
@@ -80,8 +84,8 @@ async def observation(
           await db.insert("ObservationDailyLog", (
             response.messageID, 
             'solar', 
-            date.now().strftime("%Y-%m-%d"),
-            date.now().strftime("%H:%M"),
+            datetime.now().strftime("%Y-%m-%d"),
+            datetime.now().strftime("%H:%M"),
             response.messageType, # >> FLR 과같은 이벤트 타입 
             response.messageBody,
           ))
@@ -102,15 +106,15 @@ async def observation(
           await db.insert("ObservationDailyLog",(
             response_GST.gstID,
             'earth',
-            date.now().strftime("%Y-%m-%d"),
-            date.now().strftime("%H:%M"),
+            datetime.now().strftime("%Y-%m-%d"),
+            datetime.now().strftime("%H:%M"),
             "GST(전자기 폭풍) 발생",
             f"{response_GST.startTime}에 전자기 폭풍이 발생했습니다."
           ))
           await db.insert("ObservationDailyLog",(
             response_IPS.activityID,
-            date.now().strftime("%Y-%m-%d"),
-            date.now().strftime("%H:%M"),
+            datetime.now().strftime("%Y-%m-%d"),
+            datetime.now().strftime("%H:%M"),
             "ISP(행성 간 충격) 발생",
             f"지구 근처에서 태양에서 온 충격파가 지구 주변에 도달했습니다. 태양폭발(CME)과 지자기 폭풍(GST)과 연관된 현상입니다. 데이터는 NASA의 {response_IPS.instruments.displayName} 관측 장비에서 수집되었습니다.",
           ))
@@ -130,7 +134,139 @@ async def observation_all(type: str = Query(...)):
   return await db.custom(f"SELECT * FROM ObservationDailyLog WHERE object_type={type};")
 
 # 별 위치 가져오기
-@router.get('/position')
-def observation_star():
-  return ''
+"""
+클라이언트에서 보낼값
+{
+"lat" : 37.5665, // 위도
+"lon" : 126.9780 // 경도
+"datetime": "2025-11-11T21:00:00Z" //UTC
+}
 
+서버 데이터
+[
+  {
+    "id" : 1,
+    "name": "Sirius",
+    "ra": 101.2875, // 적경 
+    "dec": -16.7161 // 적위
+    "mag": -1.46 // 밝기
+  }
+]
+"""
+class StarReq(BaseModel):
+  lat : float
+  lon : float
+  datetime : datetime
+
+class StarRes(BaseModel):
+  id: int
+  name : str
+  mag : float
+  alt : float
+  az : float
+
+# star 데이터 가져오기
+with open("stars.json", "r", encoding="utf-8") as f:
+  STARS_DATA = json.load(f)
+
+@router.post('/position', response_model=List[StarRes])
+def star_position(data : StarReq):
+
+  # 시간 -> jd
+  jd = datetime_to_julian(data.datetime)
+  # jd -> 그리니치 -> 지역 
+  # 우주 좌표 (ra/dec) >> 관측 좌표 alt/az
+  gmst = gmst_from_julian(jd)
+  lst = normalize_deg(gmst + data.lon)
+
+  visible_star = []
+
+  for star in STARS_DATA:
+    alt, az = ra_dec_to_alt_az(
+      ra_deg = star["ra"], # 도 기준
+      dec_deg= star["dec"],
+      lat_deg= data.lat,
+      lst_deg = lst
+    )
+    if alt > 0:
+      visible_star.append(
+        StarRes(
+          id = star["id"],
+          name = star["name"],
+          mag = star["mag"],
+          alt = alt,
+          az = az
+        )
+      )
+  return visible_star
+
+# == 계산함수
+def normalize_deg(d: float) -> float:
+  d = d % 360.0
+  if d < 0:
+    d += 360.0
+  return d
+
+def datetime_to_julian(date: datetime) -> float:
+  # UTC -> jd
+  if date.tzinfo is None:
+    date = date.replace(tzinfo=timezone.utc)
+  else:
+    date = date.astimezone(timezone.utc)
+
+  year = date.year
+  month = date.month
+  day = date.day + (date.hour + (date.minute + date.second / 60.0) / 60.0) / 24.0
+
+  if month <= 2:
+    year -= 1
+    month += 12
+
+  A = year // 100
+  B = 2 - A + (A // 4)
+
+  jd = int(365.25 * (year + 4716)) + int(30.6001 * (month + 1)) + day + B - 1524.5
+  return jd
+
+
+def gmst_from_julian(jd: float) -> float:
+  # Julian Day -> GMST (deg)
+  # 기준: J2000.0
+  T = (jd - 2451545.0) / 36525.0
+  gmst = 280.46061837 + 360.98564736629 * (jd - 2451545.0) + 0.000387933 * T**2 - (T**3) / 38710000.0
+  return normalize_deg(gmst)
+
+
+def deg2rad(d: float) -> float:
+  return d * pi / 180.0
+
+def rad2deg(r: float) -> float:
+  return r * 180.0 / pi
+
+def ra_dec_to_alt_az(
+    ra_deg: float, 
+    dec_deg: float, 
+    lat_deg: float, 
+    lst_deg: float
+  ):
+  # ra/dec (deg) -> alt/az (deg) 사용자 좌표로 변경
+  dec = deg2rad(dec_deg)
+  lat = deg2rad(lat_deg)
+  ha = deg2rad(normalize_deg(lst_deg - ra_deg))
+
+  # 구면삼각
+  sin_alt = sin(dec) * sin(lat) + cos(dec) * cos(lat) * cos(ha)
+  alt = asin(sin_alt)
+  cos_az = (sin(dec) - sin(alt) * sin(lat)) / (cos(alt) * cos(lat))
+
+  if cos_az > 1.0:
+    cos_az = 1.0
+  elif cos_az < -1.0:
+    cos_az = -1.0
+
+  az = acos(cos_az)
+
+  if sin(ha) > 0:
+    az = 2 * pi - az
+
+  return rad2deg(alt), rad2deg(az)
