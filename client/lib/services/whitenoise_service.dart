335b8e90b09bf269bcff7a31c09fe49399db3905
KwakJakyeong <ggyul847@gmail.com>
2025-11-20 12:44:04 +0900

    Feat: 백엔드 오디오 스트림 처리

--- client/lib/services/whitenoise_service.dart
+++ client/lib/services/whitenoise_service.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
+import 'dart:typed_data';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
-import 'package:client/models/whitenoise.dart';
+// import 'package:client/models/whitenoise.dart';
 
 class WhiteNoiseService {
   final baseUrl = dotenv.env['BASE_URL'];
@@ -17,18 +18,19 @@ class WhiteNoiseService {
     }
   }
 
-  Future<WhiteNoiseModel> fetchNextAudio({
-    required String title
+  Future<Uint8List> fetchPlayAudio({
+    required String title,
   }) async {
-    final response = await http.get(Uri.parse("$baseUrl/whitenoise/next").replace(
+    final response = await http.get(Uri.parse("$baseUrl/whitenoise/play").replace(
       queryParameters: {
         "noise_title": title
       }
     ));
 
     if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      return WhiteNoiseModel.fromJson(data);
+      // return response.bodyBytes;
+      final data = response.bodyBytes;
+      return data;
     } else {
       throw Exception("다음 오디오 데이터를 불러오지 못했습니다.");
     }
--- client/lib/viewmodels/whitenoise_viewmodel.dart
+++ client/lib/viewmodels/whitenoise_viewmodel.dart
@@ -1,8 +1,12 @@
 
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 // import 'package:client/models/whitenoise_model.dart';
 import "package:client/services/whitenoise_service.dart";
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 
 class WhiteNoiseViewModel extends ChangeNotifier {
   final WhiteNoiseService _service = WhiteNoiseService();
@@ -10,9 +14,9 @@ class WhiteNoiseViewModel extends ChangeNotifier {
 
   List<String>? _audioListData;
   List<String>? get audioListData => _audioListData;
-  ``
-  Map<String, dynamic>? _audioData;
-  Map<String, dynamic>? get audioData => _audioData;
+  
+  Uint8List? _audioData;
+  Uint8List? get audioData => _audioData;
 
   bool _loading = false;
   bool get loading => _loading;
@@ -23,41 +27,39 @@ class WhiteNoiseViewModel extends ChangeNotifier {
   Future<void> loadCurrentAudio() async {
     _loading = true;
     notifyListeners();
-
     try {
-      _audioData = await _service.fetchAudio();
+      _audioListData = await _service.fetchAudioList();
       _error = null;
     } catch (e) {
       _error = e.toString();
     }
-
     _loading = false;
     notifyListeners();
   }
 
-
-  Future<void> fetchNextAudio({
+// 플레이하는 오디오 받아오는 함수
+  Future<void> fetchPlayAudio({
     required String title
   }) async {
     _loading = true;
     notifyListeners();
-
     try {
-      _audioData = await _service.fetchNextAudio(title:title);
+      _audioData = await _service.fetchPlayAudio(title:title);
       _error = null;
     } catch (e) {
       _error = e.toString();
     }
-
     _loading = false;
     notifyListeners();
   }
 
+  Future<void> playAudio({
+    required String title
+  }) async {
+    final baseUrl = dotenv.env['BASE_URL'];
 
-  Future<void> playAudio() async {
     if (_audioData == null) return;
-
     await _player.stop();
-    await _player.play(UrlSource(_audioData!.audioUrl));
+    await _player.play(UrlSource("$baseUrl/whitenoise/play?noise_title=$title"));
   }
 }
